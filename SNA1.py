033479be8f3b74f3e3aedc2e61ab19ded5fc87fc
Taruniro <phuphinyol@gmail.com>
2024-04-17 22:36:58 +0700

    Code update

--- SNA1.py
+++ SNA1.py
@@ -2,21 +2,57 @@ import pandas as pd
 from mpl_chord_diagram import chord_diagram
 import matplotlib.pyplot as plt
 
-dfraw = pd.read_csv('OP66T.csv')
-df = dfraw[(dfraw['primary_province'] == 9600) | (dfraw['primary_province'] == 5400)
-           | (dfraw['primary_province'] == 4500) | (dfraw['primary_province'] == 7600)]
-dfs = df[['hcode', 'hmain_op']]
+dfraw = pd.read_csv('OP66T2.csv')
+df = dfraw[(dfraw['primary_province'] == 5400)]
+# 9600, 5400, 4500, 7600
+dfs = df[['hcode', 'hmain_op','hmain']]
 
 dfnar = dfs.dropna()
 dfc = dfnar.reset_index(drop=True)
 dfc['hmain_op'] = dfc['hmain_op'].astype(int)
+dfc['hmain'] = dfc['hmain'].astype(int)
 
 res = []
 for index, row in dfc.iterrows():
-    hm_op = row['hmain_op']
-    hc = row['hcode']
-    count = dfc[(dfc['hmain_op'] == hm_op) & (dfc['hcode'] == hc)].shape[0]
-    res.append((hm_op, hc, count))
+    hmain_op = row['hmain_op']
+    hcode = row['hcode']
+    hmain = row['hmain']
+    count = dfc[(dfc['hmain_op'] == hmain_op) & (dfc['hcode'] == hcode)].shape[0]
+    res.append((hmain, hmain_op, hcode, count))
+ures = []
+for item in res:
+    if item not in ures:
+        ures.append(item)
+
+dfx = pd.DataFrame(ures, columns=['hmain','hmain_op','hcode','count'])
+dfxp = dfx.pivot_table(values='count', index='hmain_op', columns='hcode',fill_value=0)
+
+column_titles = dfxp.columns
+row_titles = dfxp.index
+
+all = list(set(column_titles).union(set(row_titles)))
+s_all = all.sort()
+dfxp = dfxp.reindex(columns=all, index=all, fill_value=0)
+dflor = dfxp.values.tolist()
+hs = dfxp.columns
+
+chord_diagram(dflor, names=hs, order=None, sort="size", directed=True,
+                  colors=None, cmap=None, use_gradient=False, chord_colors=None,
+                  alpha=0.5, start_at=0, extent=360, width=0.1, pad=2., gap=0.03,
+                  chordwidth=0.7, min_chord_width=0, fontsize=5,
+                  fontcolor="k", rotate_names=True, ax=None, show=False)
+plt.show()
+
+'''
+all = list(set(column_titles).union(set(row_titles)))
+all.sort
+afall = pd.DataFrame(all, columns=['hmain_op'])
+mapraw = dfc[['hmain','hmain_op']]
+mapad = (mapraw.drop_duplicates()).reset_index(drop=True)
+
+dfmap = pd.merge(afall, mapad, on='hmain_op', how='left')
+dfso = dfmap.sort_values(by='hmain')
+dfso['hmain'] = dfso['hmain'].astype(int)
 
 dfx = pd.DataFrame(res, columns=['hm_op','hc','count'])
 dfxp = dfx.pivot_table(values='count', index='hm_op', columns='hc',fill_value=0)
@@ -30,9 +66,10 @@ dfxp = dfxp.reindex(columns=all, index=all, fill_value=0)
 dflor = dfxp.values.tolist()
 hs = dfxp.columns
 
-chord_diagram(dflor, names=hs, order=None, sort="size", directed=False,
+chord_diagram(dflor, names=hs, order=None, sort="size", directed=True,
                   colors=None, cmap=None, use_gradient=False, chord_colors=None,
                   alpha=0.5, start_at=0, extent=360, width=0.1, pad=2., gap=0.03,
                   chordwidth=0.7, min_chord_width=0, fontsize=5,
                   fontcolor="k", rotate_names=True, ax=None, show=False)
 plt.show()
+'''
--- Test.py
+++ Test.py
@@ -1,18 +1,39 @@
+import networkx as nx
 import pandas as pd
-from bokeh.charts import output_file, Chord
-from bokeh.io import show
-from bokeh.sampledata.les_mis import data
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.colors as mcolors
 
-nodes = data['nodes']
-links = data['links']
+dfraw = pd.read_csv('OP66T2.csv')
+df = dfraw[(dfraw['primary_province'] == 4500)]
+# 9600, 5400, 4500, 7600
+dfs = df[['hmain_op', 'hcode','hmain']]
 
-nodes_df = pd.DataFrame(nodes)
-links_df = pd.DataFrame(links)
+dfnar = dfs.dropna()
+dfc = dfnar.reset_index(drop=True)
+dfc['hmain_op'] = dfc['hmain_op'].astype(int)
+dfc['hmain'] = dfc['hmain'].astype(int)
+dfc['hcode'] = dfc['hcode'].astype(int)
 
-source_data = links_df.merge(nodes_df, how='left', left_on='source', right_index=True)
-source_data = source_data.merge(nodes_df, how='left', left_on='target', right_index=True)
-source_data = source_data[source_data["value"] > 5]
+G = nx.from_pandas_edgelist(dfc, 'hmain_op', 'hcode')
+degree_centrality = nx.degree_centrality(G)
+#print(degree_centrality)
 
-chord_from_df = Chord(source_data, source="name_x", target="name_y", value="value")
-output_file('chord_from_df.html', mode="inline")
-show(chord_from_df)
+unique_groups = dfc['hmain'].unique()
+num_groups = len(unique_groups)
+colors = plt.cm.rainbow(np.linspace(0, 1, num_groups))
+
+group_colors = dict(zip(unique_groups, colors))
+node_groups = dict(zip(dfc['hmain_op'], dfc['hmain']))
+node_colors = [group_colors.get(node_groups.get(node, 'default'), 'grey') for node in G.nodes()]
+
+node_size = [1000 * degree_centrality[node] for node in G.nodes()]
+plt.figure(figsize=(12, 8))
+pos = nx.spring_layout(G)
+nx.draw(G, pos, with_labels=False, node_size=node_size, node_color=node_colors, font_size=0.2,
+        font_weight='bold', edge_color='black', width=0.1)
+
+plt.title("Social Network Analysis")
+plt.show()
+
+
